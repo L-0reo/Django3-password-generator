6a5211a4558762fdcd5766bb077cee02e09cf3b1
L'oreo <l.mihaly@outlook.com>
2020-09-13 17:01:53 +0300

    ready to deploy

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from generator import views
 urlpatterns = [
@@ -21,3 +22,5 @@ urlpatterns = [
     path('generatedpassword/', views.password, name='password'),
     path('aboutwhomadethissite', views.about, name='about' )
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
