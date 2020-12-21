a09aa3e614dfda1604488777672c3851de8bf0ec
L'oreo <l.mihaly@outlook.com>
2020-12-21 21:47:23 +0200

    secrets cloaked

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -21,8 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = os.environ.get('SECRET_KEY_PASSWD_GEN')
+try:
+    SECRET_KEY = os.environ.get('SECRET_KEY_PASSWD_GEN')
+except:
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
